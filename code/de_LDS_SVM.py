62a9cd660f4696ed60b866554bb598c1ecd759c9
xiaoguowen <xiaoguowen@meituan.com>
2020-10-28 21:44:15 +0800

    修改 SVM 参数

--- code/de_LDS_SVM.py
+++ code/de_LDS_SVM.py
@@ -12,12 +12,15 @@ from sklearn.metrics import classification_report, confusion_matrix
 # 样本加载
 folder_path = '../data/ExtractedFeatures/'
 de_LDS_feature_list, de_LDS_label_list = build_extracted_features_dataset(folder_path, 'de_LDS', 'gamma')
+de_LDS_feature_list = de_LDS_feature_list
+de_LDS_label_list = de_LDS_label_list
 
 # 训练集，测试集分割
 train_feature, test_feature, train_label, test_label = train_test_split(de_LDS_feature_list, de_LDS_label_list, test_size=0.20)
 
 # SVM 分类器训练与预测
-svc_classifier = svm.SVC(kernel='linear')
+# 注意 SVC 参数设置，默认的 C 值为1，即不容错，此时搭配 linear 核很可能无法收敛。
+svc_classifier = svm.SVC(C=0.6, kernel='rbf')
 svc_classifier.fit(train_feature, train_label)
 pred_label = svc_classifier.predict(test_feature)
 print(confusion_matrix(test_label, pred_label))
