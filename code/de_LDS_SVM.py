e8840bc7c0f7846b42528a7d4119a36e5eecf8ce
xiaoguowen <xiaoguowen@meituan.com>
2020-10-30 20:47:21 +0800

    按论文的方式计算 SVM 结果

--- code/de_LDS_SVM.py
+++ code/de_LDS_SVM.py
@@ -7,21 +7,75 @@ from utils.tools import build_extracted_features_dataset
 from sklearn.model_selection import train_test_split
 from sklearn import svm
 from sklearn.metrics import classification_report, confusion_matrix
+import numpy as np
 
 
-# 样本加载
 folder_path = '../data/ExtractedFeatures/'
-de_LDS_feature_list, de_LDS_label_list = build_extracted_features_dataset(folder_path, 'de_LDS', 'gamma')
-de_LDS_feature_list = de_LDS_feature_list
-de_LDS_label_list = de_LDS_label_list
 
-# 训练集，测试集分割
-train_feature, test_feature, train_label, test_label = train_test_split(de_LDS_feature_list, de_LDS_label_list, test_size=0.20)
 
-# SVM 分类器训练与预测
-# 注意 SVC 参数设置，默认的 C 值为1，即不容错，此时搭配 linear 核很可能无法收敛。
-svc_classifier = svm.SVC(C=0.6, kernel='rbf')
-svc_classifier.fit(train_feature, train_label)
-pred_label = svc_classifier.predict(test_feature)
-print(confusion_matrix(test_label, pred_label))
-print(classification_report(test_label, pred_label))
+def all_data_svm(folder_path):
+    '''
+        加载所有被试的数据，按一定比例切分数据集，用已提取的特征进行分类，不考虑 subject-independent / subject-dependent，也不考虑被试间的差异，粗暴求平均
+    :param folder_path: ExtractedFeatures 文件夹路径
+    :return one:
+    '''
+    # 样本加载
+    de_LDS_feature_dict, de_LDS_label_dict = build_extracted_features_dataset(folder_path, 'de_LDS', 'gamma')
+    de_LDS_feature_list = []
+    de_LDS_label_list = []
+    for key in de_LDS_feature_dict.keys():
+        de_LDS_feature_list.extend(de_LDS_feature_dict[key])
+        de_LDS_label_list.extend(de_LDS_label_dict[key])
+
+    # 训练集，测试集分割
+    test_ratio = 0.4
+    train_feature, test_feature, train_label, test_label = train_test_split(de_LDS_feature_list, de_LDS_label_list,
+                                                                            test_size=test_ratio)
+
+    # SVM 分类器训练与预测
+    # 注意 SVC 参数设置，默认的 C 值为1，即不容错，此时搭配 linear 核很可能无法收敛。
+    svc_classifier = svm.SVC(C=0.7, kernel='linear')
+    svc_classifier.fit(train_feature, train_label)
+    pred_label = svc_classifier.predict(test_feature)
+    print(confusion_matrix(test_label, pred_label))
+    print(classification_report(test_label, pred_label))
+
+
+def paper_svm(folder_path):
+    '''
+        按照 SEED 数据集原始论文中的 SVM 计算方式测试准确率和方差，每个 experiment 分开计算，取其中 9 个 trial 为训练集，6 个 trial 为测试集
+    :param folder_path: ExtractedFeatures 文件夹路径
+    :return None:
+    '''
+    # 样本加载
+    de_LDS_feature_dict, de_LDS_label_dict = build_extracted_features_dataset(folder_path, 'de_LDS', 'gamma')
+    accuracy = 0
+    for key in de_LDS_feature_dict.keys():
+        print('当前处理到 experiment_{}'.format(key))
+        cur_feature = de_LDS_feature_dict[key]
+        cur_label = de_LDS_label_dict[key]
+        train_feature = []
+        train_label = []
+        test_feature = []
+        test_label = []
+        for trial in cur_feature.keys():
+            if int(trial) < 10:
+                train_feature.extend(cur_feature[trial])
+                train_label.extend(cur_label[trial])
+            else:
+                test_feature.extend(cur_feature[trial])
+                test_label.extend(cur_label[trial])
+        # 定义 svm 分类器
+        svc_classifier = svm.SVC(C=0.8, kernel='rbf')
+        svc_classifier.fit(train_feature, train_label)
+        pred_label = svc_classifier.predict(test_feature)
+        print(confusion_matrix(test_label, pred_label))
+        print(classification_report(test_label, pred_label))
+        cur_accuracy = svc_classifier.score(test_feature, test_label)
+        accuracy += cur_accuracy
+        print('当前 experiment 的 accuracy 为：{}'.format(cur_accuracy))
+
+    print('所有 experiment 上的平均 accuracy 为：{}'.format(accuracy / len(de_LDS_feature_dict.keys())))
+
+
+paper_svm(folder_path)
--- code/utils/tools.py
+++ code/utils/tools.py
@@ -46,7 +46,7 @@ def get_frequency_band_idx(frequency_band):
 
 def build_extracted_features_dataset(folder_path, feature_name, frequency_band):
     '''
-        将 folder_path 文件夹中的 ExtractedFeatures 数据转化为机器学习常用的数据集
+        将 folder_path 文件夹中的 ExtractedFeatures 数据转化为机器学习常用的数据集，区分开不同 trial 的数据
         ToDo: 增加 channel 的选择，而不是使用所有的 channel
     :param folder_path: ExtractedFeatures 文件夹对应的路径
     :param feature_name: 需要使用的特征名，如 'de_LDS'，'asm_LDS' 等，以 de_LDS1 为例，维度为 62 * 235 * 5，235为影片长度235秒，每秒切分为一个样本，62为通道数，5为频带数
@@ -68,21 +68,21 @@ def build_extracted_features_dataset(folder_path, feature_name, frequency_band):
                 if file_name not in skip_set:
                     all_features_dict = scio.loadmat(os.path.join(folder_path, file_name),
                                                      verify_compressed_data_integrity=False)
-                    subject_name = file_name.split('_')[0]
-                    feature_vector_list = []
-                    label_list = []
+                    subject_name = file_name.split('.')[0]
+                    feature_vector_trial_dict = {}
+                    label_trial_dict = {}
                     for trials in range(1, 16):
+                        feature_vector_list = []
+                        label_list = []
                         cur_feature = all_features_dict[feature_name + str(trials)]
                         cur_feature = np.asarray(cur_feature[:, :, frequency_idx]).T  # 转置后，维度为 N * 62, N 为影片长度
                         feature_vector_list.extend(_ for _ in cur_feature)
                         for _ in range(len(cur_feature)):
                             label_list.append(labels[trials - 1])
-                    if subject_name not in feature_vector_dict:
-                        feature_vector_dict[subject_name] = feature_vector_list
-                        label_dict[subject_name] = label_list
-                    else:
-                        feature_vector_dict[subject_name].extend(feature_vector_list)
-                        label_dict[subject_name].extend(label_list)
+                        feature_vector_trial_dict[str(trials)] = feature_vector_list
+                        label_trial_dict[str(trials)] = label_list
+                    feature_vector_dict[subject_name] = feature_vector_trial_dict
+                    label_dict[subject_name] = label_trial_dict
                 else:
                     continue
     except FileNotFoundError as e:
@@ -94,7 +94,7 @@ def build_extracted_features_dataset(folder_path, feature_name, frequency_band):
 def build_preprocessed_eeg_dataset_CNN(folder_path):
     '''
         预处理后的 EEG 数据维度为 62 * N，其中62为 channel 数量， N 为采样点个数（已下采样到200 Hz）
-        此函数将预处理后的 EEG 信号转化为 CNN 网络所对应的数据格式，即 62 * 200 的二维输入（每 1s 的信号作为一个样本）
+        此函数将预处理后的 EEG 信号转化为 CNN 网络所对应的数据格式，即 62 * 200 的二维输入（每 1s 的信号作为一个样本）,区分开不同 trial 的数据
     :param folder_path: Preprocessed_EEG 文件夹对应的路径
     :return feature_vector_dict, label_dict: 分别为样本的特征向量，样本的标签，key 为被试名字，val 为该被试对应的特征向量或标签的 list，方便 subject-independent 的测试
     '''
@@ -111,13 +111,15 @@ def build_preprocessed_eeg_dataset_CNN(folder_path):
                 print('当前已处理到{}，总进度{}/{}'.format(file_name, file_cnt, len(file_list)))
                 if file_name not in skip_set:
                     all_trials_dict = scio.loadmat(os.path.join(folder_path, file_name),
-                                                     verify_compressed_data_integrity=False)
-                    subject_name = file_name.split('_')[0]
-                    feature_vector_list = []
-                    label_list = []
+                                                   verify_compressed_data_integrity=False)
+                    subject_name = file_name.split('.')[0]
+                    feature_vector_trial_dict = {}
+                    label_trial_dict = {}
                     for key in all_trials_dict.keys():
                         if 'eeg' not in key:
                             continue
+                        feature_vector_list = []
+                        label_list = []
                         cur_trial = all_trials_dict[key]  # 维度为 62 * N，每200个采样点截取一个样本，不足200时舍弃
                         length = len(cur_trial[0])
                         pos = 0
@@ -126,12 +128,11 @@ def build_preprocessed_eeg_dataset_CNN(folder_path):
                             raw_label = labels[int(key.split('_')[-1][3:]) - 1]  # 截取片段对应的 label，-1, 0, 1
                             label_list.append(raw_label)
                             pos += 200
-                    if subject_name not in feature_vector_dict:
-                        feature_vector_dict[subject_name] = feature_vector_list
-                        label_dict[subject_name] = label_2_onehot(label_list)
-                    else:
-                        feature_vector_dict[subject_name].extend(feature_vector_list)
-                        label_dict[subject_name].extend(label_2_onehot(label_list))
+                        feature_vector_trial_dict[key] = feature_vector_list
+                        label_trial_dict[key] = label_2_onehot(label_list)
+
+                    feature_vector_dict[subject_name] = feature_vector_trial_dict
+                    label_dict[subject_name] = label_trial_dict
                 else:
                     continue
 
